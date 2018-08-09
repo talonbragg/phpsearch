a9c7facbe6866f1425d39686047cd7d3da013bd8
Talon Bragg <pvpgamecrash@gmail.com>
2018-08-09 10:23:13 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Hack This</title>
+  </head>
+  <body>
+    <form action="index.php" method="post">
+      <input type="text" name="search" placeholder="Search..."/>
+      <input type="submit" value=">>"/> 
+    </form>
+  </body>
+</html>
